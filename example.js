d65ec5d96d5384b30577434ef5e0017e09eff0b0
basakest <ab1652759879@gmail.com>
2019-07-14 10:33:29 +0800

    nothing

---
+++ example.js
@@ -0,0 +1,13 @@
+// mood = "happy";
+// age = 33
+var mood = "happy", age = 33;
+// alert(mood);
+// alert(age)
+var height = "about 5'10\" tall"
+//if(2 > 1)
+//alert(height)
+multiply(2, 13)
+function multiply(num1, num2) {
+	var total = num1 * num2;
+	alert(total)
+}
